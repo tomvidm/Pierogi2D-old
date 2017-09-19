5dab44b20d9f606c76112336a43e5f8835ad1b3c
Tom Vidar Michalsen <tomvidm@gmail.com>
2017-09-19 19:55:51 +0200

    Added functionality to Pool for keeping track of free and unused memory.

--- src/engine/Pool.h
+++ src/engine/Pool.h
@@ -2,15 +2,17 @@
     
 */
 
+#include <list>
+
 namespace engine
 {
     typedef unsigned int uint;
 
     enum class ObjectState
     {
-        IN_USE,
+        ACTIVE,
         FREE,
-        DEAD
+        INACTIVE
     };
 
     template <class ObjectType, uint POOL_SIZE>
@@ -21,6 +23,9 @@ namespace engine
 
         inline uint getPoolSize() const { return poolSize_; }
         inline uint getNumObjects() const { return numObjects_; }
+
+        uint activateObject();
+        void deactivateObject(uint id);
     private:
         uint poolSize_ = POOL_SIZE;
         uint numObjects_ = 0;
@@ -28,11 +33,47 @@ namespace engine
 
         ObjectState objectPoolStates_[POOL_SIZE] = { ObjectState::FREE };//*POOL_SIZE;
         ObjectType objectPool_[POOL_SIZE];
+        std::list<uint> freeList_;
     };
 
     template <class ObjectType, uint POOL_SIZE>
     Pool<ObjectType, POOL_SIZE>::Pool()
     {
         ;
+    }
+
+    template <class ObjectType, uint POOL_SIZE>
+    uint Pool<ObjectType, POOL_SIZE>::activateObject()
+    {
+        if (!freeList_.empty())
+        {
+            uint index = freeList_.front()
+            objectPoolStates_[index] = ObjectState::ACTIVE;
+            freeList_.pop_front();
+            numObjects_ += 1;
+            return index;
+
+        }
+        else if (numObjects_ < poolSize_)
+        {
+            objectPoolStates_[firstFreeObject_] = ObjectState::ACTIVE;
+            numObjects += 1;
+            return firstFreeObject_++;
+        }
+        else
+        {
+            return -1; // No more space
+        }
+    }
+
+    template <class ObjectType, uint POOL_SIZE>
+    void Pool<ObjectType, POOL_SIZE>::deactivateObject(uint id)
+    {
+        if (objectPoolStates_[id] == ObjectState::ACTIVE)
+        {
+            objectPoolStates_[id] == ObjectState::INACTIVE;
+            freeList_.push_back(id);
+            numObjects_ -= 1;
+        }
     }
 }
