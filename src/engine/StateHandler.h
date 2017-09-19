779a0cd10e6e70b55146e4f744af0f30f5708095
Tom Vidar Michalsen <tomvidm@gmail.com>
2017-09-19 16:17:10 +0200

    Added a few notes.

--- src/engine/StateHandler.h
+++ src/engine/StateHandler.h
@@ -2,6 +2,14 @@
 	The StateHandler handles states, taking care of pushing
 	and popping states as needed. It holds a stack of shared pointers
 	to base states to enable polymorphic behaviour.
+
+	The interface should be common enough so that the state handler can be a
+	general purpose handler for both the main game loops and a
+	handler for individual objects and Ai's.
+
+	TODO: 	* How should knowledge about other parts of the system be passed to the state handler
+		  	and states?
+		  	* How to effectively implement polymorphism? HODEPINE JAAA
 */
 
 #include <memory>
