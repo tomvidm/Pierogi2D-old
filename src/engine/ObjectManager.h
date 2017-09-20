f32f6a36296276aa6c70daca3b0c8f016fb9bc22
Tom Vidar Michalsen <tomvidm@gmail.com>
2017-09-20 15:25:49 +0200

    Work on Objectmanager. Started on SceneNode

--- src/engine/ObjectManager.h
+++ src/engine/ObjectManager.h
@@ -1,5 +1,6 @@
 #include "Pool.h"
 #include "Tree.h"
+#include "SceneNode.h"
 
 #define MAX_OBJECTS 64
 #define MAX_SPRITES 64
@@ -7,18 +8,26 @@
 namespace engine
 {
     typedef unsigned int uint;
-    typedef int SomeObject;
-    typedef int Sprite;
-    typedef Pool<SomeObject, MAX_OBJECTS> ObjectPool; 
-    typedef Pool<Sprite, MAX_SPRITES> SpritePool;
+    typedef Pool<SceneNode, MAX_OBJECTS> SceneNodePool;
 
-    class SceneManager
+    /*
+        For data coherence, updating each node by iterating through the array
+        is desirable. If a node has a set of children, we want to update these children by updating the parent first,
+        and maybe ignore the children until the parent is found.
+    */
+    enum class UpdateMode
+    {
+        IGNORE_SUBTREE,
+        UPDATE_SUBTREE,
+        UPDATE_SUBTREE_IF_ROOT
+    };
+
+    class ObjectManager
     {
     public:
-        
+        void update(uint index, UpdateMode updateMode = UpdateMode::IGNORE_SUBTREE);
     private:
-        utils::Tree<uint> objectTree_;
-        ObjectPool objectPool_;
-        SpritePool spritePool_;
+        utils::Tree<uint> sceneNodeTree_;
+        SceneNodePool sceneNodePool_;
     };
 }
---
+++ src/engine/SceneNode.h
@@ -0,0 +1,11 @@
+
+namespace engine
+{
+
+
+    class SceneNode
+    {
+    private:
+        int i;
+    }
+}
