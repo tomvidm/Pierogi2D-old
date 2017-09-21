6b4c56577ca10803fe2f871eba33c5b80808584e
Tom Vidar Michalsen <tomvidm@gmail.com>
2017-09-21 21:25:36 +0200

    Copypasta and tweaking for automatic inclusion of sfml with cmake

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,19 @@
+#include <SFML/Graphics.hpp>
+
 int main(int argc, char* argv[])
 {
-	return 0;
+    sf::Window App(sf::VideoMode(800, 600), "myproject");
+
+    while (App.isOpen()) 
+    {
+        sf::Event Event;
+        while (App.pollEvent(Event)) 
+        {
+            if (Event.type == sf::Event::Closed) 
+            {
+                App.close();
+            }
+        }
+    }
+    App.display();
 }
